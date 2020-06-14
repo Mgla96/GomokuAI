3e04282d8cc2f16645fbdb7a62e8fdc94b809fd1
mgla96 <mattgottlieb96@gmail.com>
2020-06-14 09:53:09 -0700

    save

---
+++ gobang.py
@@ -0,0 +1,394 @@
+import sys
+import time
+import math
+import numpy as np
+import random
+'''
+Gomoku AI
+Using MiniMax with Alpha Beta Pruning 
+Alpha is best value maximizer currently can guarantee at that level or above
+Beta is best value that minimizer currently can guarantee at that level or above
+board[row][column]
+'''
+
+def getCoordsAround(board_size, board):
+    '''
+    get points around placed stones
+    getCoordsAroundTime 0.00011587142944335938 sometimes
+    '''
+    outTpl = np.nonzero(board)  # return tuple of all non zero points on board
+    potentialValsCoord = {}
+    len1 = len(outTpl[0])
+    for i in range(len1):
+        y = outTpl[0][i]
+        x = outTpl[1][i]
+        if(y > 0):
+            potentialValsCoord[(x, y-1)] = 1
+            if(x > 0):
+                potentialValsCoord[(x-1, y-1)] = 1
+            if(x < (board_size-1)):
+                potentialValsCoord[(x+1, y-1)] = 1
+        if(x > 0):
+            potentialValsCoord[(x-1, y)] = 1
+            if(y < (board_size-1)):
+                potentialValsCoord[(x-1, y+1)] = 1
+        if(y < (board_size-1)):
+            potentialValsCoord[(x, y+1)] = 1
+            if(x < (board_size-1)):
+                potentialValsCoord[(x+1, y+1)] = 1
+            if(x > 0):
+                potentialValsCoord[(x-1, y+1)] = 1
+        if(x < (board_size-1)):
+            potentialValsCoord[(x+1, y)] = 1
+            if(y > 0):
+                potentialValsCoord[(x+1, y-1)] = 1
+    finalValsX, finalValsY = [], []
+    for key in potentialValsCoord:
+        finalValsX.append(key[0])
+        finalValsY.append(key[1])
+    return finalValsX, finalValsY
+
+
+def convertArrToMove(row, col):
+    '''
+    col goes to letter of number plus 1
+    row is number but plus 1
+    '''
+    colVal = chr(col+ord('a'))  # 97
+    rowVal = str(row+1)
+    return colVal+rowVal
+
+
+def convertMoveToArr(col, row):
+    '''
+    convert move ex: a4 to be converted to col and row integers for array
+    '''
+    colVal = ord(col)-ord('a')  # double check
+    rowVal = int(row)-1
+    return colVal, rowVal
+
+
+def convertKeyToArr(key):
+    '''
+    convert key in getcoordsaround func to array indexes
+    '''
+    colVal = ord(key[0])-ord('a')  # double check
+    rowVal = int(key[1:])-1
+    return colVal, rowVal
+
+
+def getRandomMove(board, boardSize):
+    '''
+    For choosing random move when can't decide propogated to center
+    '''
+    ctr = 0
+    idx = math.floor(boardSize/2)
+    while ctr < (idx/2):
+        if(board[idx+ctr][idx+ctr]) == 0:
+            return idx+ctr, idx+ctr
+        elif(board[idx+ctr][idx-ctr]) == 0:
+            return idx+ctr, idx-ctr
+        elif(board[idx+ctr][idx]) == 0:
+            return idx+ctr, idx
+        elif(board[idx][idx+ctr]) == 0:
+            return idx, idx+ctr
+        elif(board[idx][idx-ctr] == 0):
+            return idx, idx-ctr
+        elif(board[idx-ctr][idx] == 0):
+            return idx-ctr, idx
+        elif(board[idx-ctr][idx-ctr] == 0):
+            return idx-ctr, idx-ctr
+        elif(board[idx-ctr][idx+ctr] == 0):
+            return idx-ctr, idx+ctr
+        ctr += 1
+    for i in range(boardSize):
+        for j in range(boardSize):
+            if(board[i][j] == 0):
+                return i, j
+
+
+def btsConvert(board, player):
+    '''
+    convert board to col,row,and diag string arrays for easier interpreting 
+    '''
+    cList, rList, dList = [], [], []
+    bdiag = [board.diagonal(i) for i in range(
+        board.shape[1]-5, -board.shape[1]+4, -1)]
+    fdiag = [board[::-1, :].diagonal(i)
+             for i in range(board.shape[1]-5, -board.shape[1]+4, -1)]
+    for dgd in bdiag:
+        bdiagVals = ""
+        for point in dgd:
+            if point == 0:
+                bdiagVals += "0"
+            elif point == player:
+                bdiagVals += "1"
+            else:
+                bdiagVals += "2"
+        dList.append(bdiagVals)
+    for dgu in fdiag:
+        fdiagVals = ""
+        for point in dgu:
+            if point == 0:
+                fdiagVals += "0"
+            elif point == player:
+                fdiagVals += "1"
+            else:
+                fdiagVals += "2"
+        dList.append(fdiagVals)
+    boardT = board.copy().transpose()
+    for col in boardT:
+        colVals = ""
+        for point in col:
+            if point == 0:
+                colVals += "0"
+            elif point == player:
+                colVals += "1"
+            else:
+                colVals += "2"
+        cList.append(colVals)
+    for row in board:
+        rowVals = ""
+        for point in row:
+            if point == 0:
+                rowVals += "0"
+            elif point == player:
+                rowVals += "1"
+            else:
+                rowVals += "2"
+        rList.append(rowVals)
+    return dList+cList+rList
+
+
+def points(board, player):  # evaluates
+    '''
+    assigns points for moves
+    time for 11 board: ~0.0012657642364501953 or less thanks to dictionaries
+    '''
+    val = 0
+    player1StrArr = btsConvert(board, player)
+    tot = len(player1StrArr)
+    for i in range(tot):
+        len1 = len(player1StrArr[i])
+        for j in range(len1):
+            n = j+5
+            if(n <= len1):
+                st = player1StrArr[i][j:n]
+                if(st in patterns):
+                    val += patterns[st]
+        for j in range(len1):
+            n = j+6
+            if(n <= len1):
+                st = player1StrArr[i][j:n]
+                if(st in patterns):
+                    val += patterns[st]
+    return val
+
+
+def otherPlayerStone(player):
+    '''
+    Stones are 1 or 2 based on player. Just gets other player's stone
+    '''
+    if(player == 1):
+        return 2
+    else:
+        return 1
+
+
+def minimax(board, isMaximizer, depth, alpha, beta, player):  # alpha, beta
+    '''
+    Minimax with Alpha-Beta pruning (also computer is 1st Max in this implementation)
+    alpha is best already explored option along path to root for maximizer(AI)
+    beta is best already explored option along path to root for minimizer(AI Opponent)
+    '''
+    point = points(board, player)
+    if(depth == 2 or point>=20000000 or point<=-20000000):
+        return point
+    size = len(board)
+    if(isMaximizer):  # THE MAXIMIZER
+        best = MIN
+        potentialValsX, potentialValsY = getCoordsAround(size, board)
+        len1 = len(potentialValsX)
+        for i in range(len1):
+            if (board[potentialValsY[i]][potentialValsX[i]] == 0):
+                board[potentialValsY[i]][potentialValsX[i]] = player
+                score = minimax(board, False, depth+1, alpha, beta, player)
+                best = max(best, score)
+                alpha = max(alpha, best)  # best AI Opponent move
+                board[potentialValsY[i]][potentialValsX[i]] = 0  # undoing
+                if beta <= alpha:
+                    break
+        return best
+    else:  # THE MINIMIZER
+        best = MAX
+        potentialValsX, potentialValsY = getCoordsAround(size, board)
+        len1 = len(potentialValsX)
+        for i in range(len1):
+            if (board[potentialValsY[i]][potentialValsX[i]] == 0):
+                otherplayer = otherPlayerStone(player)
+                board[potentialValsY[i]][potentialValsX[i]] = otherplayer
+                score = minimax(board, True, depth+1, alpha, beta, player)
+                best = min(best, score)
+                beta = min(beta, best)  # best AI Opponent move
+                board[potentialValsY[i]][potentialValsX[i]] = 0  # undoing
+                if beta <= alpha:
+                    break
+        return best
+
+
+def computer(board_size, board, isComputerFirst):
+    '''
+    Chooses best move for computer
+    Max that gives index and calls min in minimax 
+    '''
+    mostPoints = -math.inf
+    alpha = MIN
+    beta = MAX
+    if(isComputerFirst):
+        mark = 1
+    else:
+        mark = 2
+    bestMoveRow = -1
+    bestMoveCol = -1
+    boardSize = len(board)
+    potentialValsX, potentialValsY = getCoordsAround(board_size, board)
+    len1 = len(potentialValsX)
+    for i in range(len1):
+        if(board[potentialValsY[i]][potentialValsX[i]] == 0):
+            board[potentialValsY[i]][potentialValsX[i]] = mark
+            movePoints = max(mostPoints, minimax(
+                board, False, 1, alpha, beta, mark))
+            alpha = max(alpha, movePoints)
+            board[potentialValsY[i]][potentialValsX[i]] = 0
+            if(beta <= alpha):
+                break
+            if(movePoints > mostPoints):
+                bestMoveRow = potentialValsY[i]
+                bestMoveCol = potentialValsX[i]
+                mostPoints = movePoints
+                if(movePoints >= 20000000):
+                    break
+    if(bestMoveRow == -1 or bestMoveCol == -1):  # ' when still -1
+        bestMoveRow, bestMoveCol = getRandomMove(board, boardSize)
+    board[bestMoveRow][bestMoveCol] = mark
+    move = convertArrToMove(bestMoveRow, bestMoveCol)  # row,col
+    return move, board
+
+
+def playGame(humanFirst, board_size, board):
+    '''
+    basic structure of game
+    '''
+    playing = True
+    moveNum = 0
+    while playing:
+        if(humanFirst):  # human dark so goes first
+            while True:
+                val = input("move:")
+                col = val[0]  # letter
+                row = val[1:]  # number
+                if(col.isalpha() and row.isalnum()):
+                    colVal, rowVal = convertMoveToArr(col, row)
+                    if colVal < board_size and colVal >= 0 and rowVal < board_size and rowVal >= 0 and board[rowVal][colVal] == 0:
+                        board[rowVal][colVal] = 1
+                        print(board)
+                        break  # entered correct value
+                    else:
+                        print("Error invalid move: please try again")
+                else:
+                    print("Error invalid move: please try again")
+            # then computer
+            print("Move played:", val)
+            moveNum += 1
+            if(moveNum == 1):
+                moveArr = getRandomMove(board, board_size)
+                move = convertArrToMove(moveArr[0], moveArr[1])
+                board[moveArr[0], moveArr[1]] = 2
+            else:
+                move, board = computer(board_size, board, False)
+            print("Move played:", move)
+            print(board)
+        else:  # human light so computer first
+            # computer first
+            if(moveNum == 0):  # first move best off just placing in center of board
+                idx = math.floor(board_size/2)
+                if(board_size % 2 == 0):
+                    board[idx-1][idx-1] = 1
+                    move = convertArrToMove(idx-1, idx-1)
+                else:
+                    board[idx][idx] = 1
+                    move = convertArrToMove(idx, idx)
+                moveNum += 1
+            else:
+                move, board = computer(board_size, board, True)
+            print("Move played:", move)
+            print(board)
+            # then human
+            while True:
+                val = input("move:")
+                col = val[0]  # letter
+                row = val[1:]  # number
+                if(col.isalpha() and row.isalnum()):
+                    colVal, rowVal = convertMoveToArr(col, row)
+                    if colVal < board_size and colVal >= 0 and rowVal < board_size and rowVal >= 0 and board[rowVal][colVal] == 0:
+                        board[rowVal][colVal] = 2
+                        print(board)
+                        break  # entered correct value
+                    else:
+                        print("Error invalid move: please try again")
+                else:
+                    print("Error invalid move: please try again")
+            print("Move played:", val)
+
+
+if __name__ == "__main__":
+    '''
+    dark color is 1, light color is 2
+    process command line arguments 
+    -n size
+    -l human opponent plays with light colors. If not specified, human opponent plays with dark colors
+    dark player always moves first
+    '''
+    humanFirst = True  # human plays with dark color so human goes first
+    size = 11  # if size not specified default 11*11
+    for i in range(1, len(sys.argv)):
+        if(sys.argv[i] == "-n"):
+            size = sys.argv[i+1]
+        elif(sys.argv[i] == "-l"):
+            humanFirst = False  # human plays with light colors so computer goes first
+    board = np.zeros((int(size), int(size)))
+    # for alpha beta
+    MAX, MIN = math.inf, -math.inf
+    patterns = {
+        '11111': 30000000,
+        '22222': -30000000,
+        '011110': 20000000,
+        '022220': -20000000,
+        '011112': 50000,
+        '211110': 50000,
+        '022221': -50000,
+        '122220': -50000,
+        '01110': 30000,
+        '02220': -30000,
+        '011010': 15000,
+        '010110': 15000,
+        '022020': -15000,
+        '020220': -15000,
+        '001112': 2000,
+        '211100': 2000,
+        '002221': -2000,
+        '122200': -2000,
+        '211010': 2000,
+        '210110': 2000,
+        '010112': 2000,
+        '011012': 2000,
+        '122020': -2000,
+        '120220': -2000,
+        '020221': -2000,
+        '022021': -2000,
+        '01100': 500,
+        '00110': 500,
+        '02200': -500,
+        '00220': -500
+    }
+    playGame(humanFirst, int(size), board)
---
+++ referee.py
@@ -0,0 +1,496 @@
+#!/usr/bin/env python3
+
+"""Referee program for gobang
+
+Written by previous TA for CS 165A, Winter 2016 ... Winter 2017
+Updated by Karl Wang for CS 165A, Spring 2020
+
+  Usage:
+    referee.py board_size path_to_light_player path_to_dark_player
+
+  Typical usage example:
+    ./referee.py 11 ./gobang1 ./gobang2
+"""
+
+import string
+import sys
+import os
+import subprocess
+import threading
+import signal
+import re
+import time
+import psutil
+import argparse
+
+
+#TIME = 31.0      # Allowed maximum time for a movement
+TIME = 100.0
+TOTAL_TIME_LIMIT = 121
+EMPTY = 0
+DARK = 1
+LIGHT = 2
+CHAINLEN = 5
+MOVE_REGEX = re.compile(b"[m|M]ove [p|P]layed[ \t]*:[ \t]*[a-z][0-9]{1,2}\n")
+
+class GobangGame:
+  board = None
+  board_size = None
+
+  dark_player = None
+  light_player = None
+  winner = EMPTY 
+  moves = 0
+
+  def __init__(self, board_size, dark_player, light_player):
+    self.board_size = board_size
+    self.board = [[EMPTY for x in range(0, board_size)] for x in range(0, board_size)]
+
+    self.dark_player = dark_player
+    self.light_player = light_player
+    winner = EMPTY
+    moves = 0
+
+  def start_game(self):
+    self.print_board()
+    self.dark_player.execute(self.board_size)
+    self.light_player.execute(self.board_size)
+
+  def print_board(self):
+    sys.stdout.write("  ")
+    i = 0
+    for c in string.ascii_lowercase:
+      i += 1
+      if i > self.board_size:
+        break
+      sys.stdout.write("   %s" % c)
+    sys.stdout.write("\n   +")
+    for i in range(0, self.board_size):
+      sys.stdout.write("---+")
+    sys.stdout.write("\n")
+
+    for i in range(0, self.board_size):
+      sys.stdout.write("%2d |" % (i + 1))
+      for j in range(0, self.board_size):
+        if self.board[i][j] == LIGHT:
+          sys.stdout.write(" L |")
+        elif self.board[i][j] == DARK:
+          sys.stdout.write(" D |")
+        else:
+          sys.stdout.write("   |")
+      sys.stdout.write("\n   +")
+      for j in range(0, self.board_size):
+        sys.stdout.write("---+")
+      sys.stdout.write("\n")
+
+  # Print results
+  def print_results(self):
+    if self.winner == DARK:
+      print("Dark player wins!")
+      kill_game(DARK)
+    elif self.winner == LIGHT:
+      print("Light player wins!")
+      kill_game(LIGHT)
+    else:
+      print("It's a tie!")
+      kill_game(0)
+
+  def apply_move(self, row, column, color):
+    score = self.update_board(row, column, color)
+    self.moves += 1
+    self.updateGameStatus(row, column, color)
+
+  def is_game_over(self):
+    if self.moves == self.board_size*self.board_size or self.winner != EMPTY:
+      return True
+    return False
+
+  def parse_move(self, move):
+    if not move:
+      return None
+
+    # Parse move
+    try:
+      column = ord(move[0]) - ord('a')
+      row = int(move[1:]) - 1
+    except ValueError:
+      return None
+    except TypeError:
+      return None
+    return row, column
+
+  def is_valid_move(self, row, column, color):
+
+    # Check boundaries
+    if row < 0 or row > self.board_size - 1 or column < 0 or column > self.board_size - 1:
+      return False
+
+    # Tile must be empty
+    if self.board[row][column] != EMPTY:
+      return False
+
+    return True
+
+  def update_board(self, row, column, color):
+    self.board[row][column] = color
+    
+  def updateGameStatus(self, row, col, color):
+    count1 = 0
+    for i in range(col, self.board_size):
+      if self.board[row][i] == color:
+        count1 += 1
+      else:
+        break
+    count2 = 0
+    for i in range(col, -1, -1):
+      if self.board[row][i] == color:
+        count2 += 1
+      else:
+        break
+    if count1 + count2 - 1 >= CHAINLEN:
+      self.winner = color
+      return
+
+    count1 = 0
+    for i in range(row, self.board_size):
+      if self.board[i][col] == color:
+        count1 += 1
+      else:
+        break
+    count2 = 0
+    for i in range(row, -1, -1):
+      if self.board[i][col] == color:
+        count2 += 1
+      else:
+        break
+    if count1 + count2 - 1 >= CHAINLEN:
+      self.winner = color
+      return
+
+    count1 = 0
+    i = row
+    j = col
+    while i >= 0 and j >= 0:
+      if self.board[i][j] == color:
+        count1 += 1
+      else:
+        break
+      i -= 1
+      j -= 1
+    count2 = 0
+    i = row
+    j = col
+    while i < self.board_size and j < self.board_size:
+      if self.board[i][j] == color:
+        count2 += 1
+      else:
+        break
+      i += 1
+      j += 1
+    if count1 + count2 - 1 >= CHAINLEN:
+      self.winner = color
+      return
+
+    count1 = 0
+    i = row
+    j = col
+    while i >=0 and j < self.board_size:
+      if self.board[i][j] == color:
+        count1 += 1
+      else:
+        break
+      i -= 1
+      j += 1      
+    count2 = 0
+    i = row
+    j = col
+    while i < self.board_size and j >= 0:
+      if self.board[i][j] == color:
+        count2 += 1
+      else:
+        break
+      i += 1
+      j -= 1
+    if count1 + count2 - 1 >= CHAINLEN:
+      self.winner = color
+    
+
+#######################################################################
+
+class GobangPlayer:
+  score = None
+  lost_turns = None
+  color = None
+  name = None
+  executable_path = None
+  executable = None
+  timer = None
+  stdoutput = None
+  total_time = 0
+
+  def __init__(self, color, name, executable_path):
+    self.score = 2
+    self.lost_turns = 0
+    if color == DARK:
+      self.lost_turns = -1
+    self.color = color
+    self.name = name
+    self.executable_path = executable_path
+
+  def kill(self):
+    if self.timer:
+      self.timer.cancel()
+    pid = int(self.executable.pid)
+
+    # Find process children (if any) and kill them
+    try:
+      for child in psutil.Process(pid).children(recursive = True):
+        try:
+          os.kill(child.pid, signal.SIGTERM)
+        except:
+          pass
+    except:
+      pass
+
+    try:
+      self.executable.stdin.close()
+    except:
+      pass
+    try:
+      self.executable.kill()
+    except:
+      pass
+    try:
+      self.executable.terminate()
+    except:
+      pass
+    try:
+      os.kill(pid, signal.SIGTERM)
+    except:
+      pass
+    self.stdoutput.close()
+
+  # Execute the program of the player
+  def execute(self, board_size):
+#    try:
+      # Call executable
+      argument_list = ["-n", str(board_size)]
+      # If the program is going to be the dark player we need to pass the -l argument
+      if self.color == DARK:
+        argument_list.append("-l")
+      self.executable = subprocess.Popen([self.executable_path] + argument_list, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+      self.stdoutput = open(str(self.name) + ".out", "w")
+#    except OSError:
+#      print("Error: Could not execute %s (%s player)" % #(self.executable_path, self.name))
+#      sys.exit(0)
+#    except ValueError:
+#      print("Error: Could not execute %s (%s player)" % #(self.executable_path, self.name))
+#      sys.exit(0)
+
+  # Start timer for player's next move
+  def start_timer(self):
+    # If the time limit is reached, terminate the game and have the other player win
+    self.timer = threading.Timer(TIME, terminate_game, [self.color])
+    self.timer.setDaemon(True)
+    self.timer.start()
+
+  # Stop timer for player's next move
+  def stop_timer(self):
+    self.timer.cancel()
+    self.timer.join()
+
+  # Player lost their turn
+  def lost_turn(self):
+    self.lost_turns += 1
+
+  # Get and parse the player's next move
+  def get_next_move(self, other_player_lost_turns):
+    move = None
+
+    # Log time to print how much time it took to get the next move
+    time_begin = time.time()
+    # Start timer
+    self.start_timer()
+    counter = 0
+    # Keep reading from the pipe until the current player's move is processed
+    while True:
+      line = self.executable.stdout.readline()
+      self.stdoutput.write(str(line.decode("utf-8")))
+      self.stdoutput.flush()
+      if MOVE_REGEX.match(line):
+        # print("matched, other_player_lost_turns: {}, self.lost_turns {}, counter {}".format(other_player_lost_turns, self.lost_turns, counter))
+        if other_player_lost_turns > 0:
+          move = str(line[:-1])
+          break
+        counter += 1
+      if counter == 2 + self.lost_turns:
+        move = str(line[:-1])
+        break
+    # Stop timer when move is accepted
+    self.stop_timer()
+    time_end = time.time()
+    move_time = time_end - time_begin
+    self.total_time += move_time
+
+    if self.total_time > TOTAL_TIME_LIMIT:
+      print("Error: %s player reached the total time limit of 2 minutes per game" % self.name)
+      print("Game over!")
+      if self.color == DARK:
+        print("Light player wins!")
+        kill_game(LIGHT)
+      else:
+        print("Dark player wins!")
+        kill_game(DARK)
+
+    # Reset lost turn counter
+    self.lost_turns = 0
+
+    try:
+      move_temp = move.split()[2]
+      if move_temp == ":":
+        move = move.split()[3][:-1]
+      else:
+        if sys.version_info[0] == 2:
+          move = move_temp
+        else:
+          move = move_temp[:-1]
+    except IndexError:
+      print("Error: Wrong format of move: %s" % move)
+      return None
+
+    return move, move_time
+
+  def send_next_move(self, move):
+    try:
+      if sys.version_info[0] == 2:
+        self.executable.stdin.write("%s\n" % move)
+      else:
+        self.executable.stdin.write(bytes("%s\n" % move, "UTF-8"))
+        self.executable.stdin.flush()
+    except ValueError:
+      terminate_game2(self.color)
+
+#######################################################################
+
+game = None
+
+def main():
+  global game
+
+  parser = argparse.ArgumentParser(description='Gobang referee for CS 165A')
+  parser.add_argument('board_size', metavar='N', type=int, help='the size of the board')
+  parser.add_argument('light_executable_path', help='path to light player\'s executable')
+  parser.add_argument('dark_executable_path', help='path to dark player\'s executable')
+
+  args = parser.parse_args()
+
+  board_size = args.board_size
+  if  board_size < 5 or board_size > 26:
+    print("Error: Board size must be an even integer between 4 and 26")
+    sys.exit(0)
+
+  light_executable_path = args.light_executable_path
+  if not os.path.exists(light_executable_path) or not os.access(light_executable_path, os.X_OK):
+    print("Error: File %s does not exist or is not executable (light player)" % light_executable_path)
+    sys.exit(0)
+
+  dark_executable_path = args.dark_executable_path
+  if not os.path.exists(dark_executable_path) or not os.access(dark_executable_path, os.X_OK):
+    print("Error: File %s does not exist or is not executable (dark player)" % dark_executable_path)
+    sys.exit(0)
+
+  print("Board size: %d" % board_size)
+  print("Path to the executable of the light player: %s" % light_executable_path)
+  print("Path to the executable of the dark player: %s" % dark_executable_path)
+
+  dark_player = GobangPlayer(DARK, "Dark", dark_executable_path)
+  light_player = GobangPlayer(LIGHT, "Light", light_executable_path)
+
+  game = GobangGame(board_size, dark_player, light_player)
+  game.start_game()
+
+  # Start gameplay
+  try:
+    current_player = light_player # Set to light player so it switches to dark in the first turn
+    while True:
+      # Swap players in every turn
+      if current_player == light_player:
+        current_player = dark_player
+        other_player = light_player
+      else:
+        current_player = light_player
+        other_player = dark_player
+
+      if True:
+        print("%s player's turn" % current_player.name)
+        move, move_time = current_player.get_next_move(other_player.lost_turns)
+
+        if game.parse_move(move):
+          row, column = game.parse_move(move)
+        else:
+          print("Error: %s player tried an invalid move (%s)" % (current_player.name, move))
+          print("Game over!")
+          print("%s player wins!" % other_player.name)
+          kill_game(other_player.color)
+
+        if not game.is_valid_move(row, column, current_player.color):
+          print("Error: %s player tried an invalid move (%s)" % (current_player.name, move))
+          print("Game over!")
+          print("%s player wins!" % other_player.name)
+          kill_game(other_player.color)
+
+        print("%s player's move: %s (within %.2f seconds)" % (current_player.name, move, move_time))
+
+        game.apply_move(row, column, current_player.color)
+        game.print_board()
+
+        if game.is_game_over():
+          other_player.send_next_move(move)
+          break
+
+        # Send current player's move to the other player
+        other_player.send_next_move(move)
+      else:
+        print("%s player has no valid moves" % current_player.name)
+  except KeyboardInterrupt:
+    print("Error: KeyboardInterrupt captured")
+    kill_game(0)
+  #except:
+  #  print("Error: Unexpected exception occurred")
+  #  kill_game()
+
+  print("Game over!")
+  game.print_results()
+
+
+def terminate_game(color):
+  print("Error: Current player takes too long to decide on their next move (%d seconds passed)" % TIME)
+  print("Game over!")
+  if color == DARK:
+    print("Light player wins!")
+    kill_game(LIGHT)
+  else:
+    print("Dark player wins!")
+    kill_game(DARK)
+
+def terminate_game2(color):
+  print("Error: Next player is not responsive")
+  print("Game over!")
+  if color == DARK:
+    print("Light player wins!")
+    kill_game(LIGHT)
+  else:
+    print("Dark player wins!")
+    kill_game(DARK)
+
+def kill_game(color):
+  print("Exiting...")
+  game.dark_player.kill()
+  game.light_player.kill()
+  sys.stdout.flush()
+  sys.stderr.flush()
+  os._exit(color)
+
+
+if __name__ == "__main__":
+    main()
